a16663991772820ff380ee34a1bd79ba71cb9f0d
bjw446 <bjw446@naver.com>
2026-03-01 22:45:23 +0900

    refactor : findByIdWithUser 메서드 JPQL -> QueryDSL로 변경

--- src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
+++ src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
@@ -10,18 +10,12 @@ import org.springframework.data.repository.query.Param;
 import java.time.LocalDateTime;
 import java.util.Optional;
 
-public interface TodoRepository extends JpaRepository<Todo, Long> {
+public interface TodoRepository extends JpaRepository<Todo, Long>, TodoCustomRepository {
 
     @Query("SELECT t FROM Todo t LEFT JOIN FETCH t.user u ORDER BY t.modifiedAt DESC")
     Page<Todo> findAllByOrderByModifiedAtDesc(Pageable pageable);
 
-    @Query("SELECT t FROM Todo t " +
-            "LEFT JOIN t.user " +
-            "WHERE t.id = :todoId")
-    Optional<Todo> findByIdWithUser(@Param("todoId") Long todoId);
-
-
-    // Todo에 weather 및 기간의 시작과 끝 조건이 Null 또는 값을 가지고 있는지 검색
+    // 투두에 weather 및 기간의 시작과 끝 조건이 Null 또는 값을 가지고 있는지 검색
     // modifiedAt 기준 기간의 시작과 끝 조건 설정
     // 기존 OrderBy 동일하게 추가
     @Query("SELECT t FROM Todo t LEFT JOIN FETCH t.user " +
