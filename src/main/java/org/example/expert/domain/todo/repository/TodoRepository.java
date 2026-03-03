4c683096019c13656c7fd2bb2d76396401c7344f
bjw446 <bjw446@naver.com>
2026-03-03 18:56:02 +0900

    test : 일정 검색 기능 테스트

--- src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
+++ src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
@@ -1,6 +1,7 @@
 package org.example.expert.domain.todo.repository;
 
 import org.example.expert.domain.todo.entity.Todo;
+import org.example.expert.domain.user.entity.User;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
